4b4d9d55f7c498e64aca99f369e2055e19936104
devjihad <mdhajihad609@gmail.com>
2026-03-06 11:31:04 +0600

    education section all done

--- src/Container/Education/Education.jsx
+++ src/Container/Education/Education.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import line from '../../assets/custom-line.png'
+import leftImg from '../../assets/Container.png'
+import team from '../../assets/Tpm Our Supported Company Area Start.png'
 const Education = () => {
     return (
         <div className='container mt-30'>
@@ -31,6 +33,30 @@ const Education = () => {
                 </div>
 
             </div>
+
+            <div className='flex gap-7.5 mt-18.25'>
+                <div className='w-1/2'>
+                    <img src={leftImg} alt=""/>
+                </div>
+
+                <div className=' w-1/2 pl-10 pr-34.5'>
+                    <h4 className=' text-[32px] font-rajdhani font-bold flex items-center gap-5'>Experience <img src={line} alt="" /></h4>
+                    <div className='my-7.5'>
+                        <p className='text-[16px] text-red1 font-rubik font-medium'>experience</p>
+                        <h4 className='text-[24px] font-rajdhani font-bold '>Fatima Asrafy</h4>
+                        <p className='text-[18px] text-[#FFFFFF] font-rajdhani font-medium'>UI/UX Designen</p>
+                        <p className='mt-2 text-gray1 text-[16px] font-rubik'>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum desi dolore eu fugiat nulla pariatu Duis aute irure</p>
+                    </div>
+
+                    <div>
+                        <p className='text-[16px] text-red1 font-rubik font-medium'>experience</p>
+                        <h4 className='text-[24px] font-rajdhani font-bold '>Fatima Asrafy</h4>
+                        <p className='text-[18px] text-[#FFFFFF] font-rajdhani font-medium'>UI/UX Designen</p>
+                        <p className='mt-2 text-gray1 text-[16px] font-rubik'>Interested in working together? Lets bring your ideas tolife! Contact me, and lets start building something.</p>
+                    </div>
+                </div>
+            </div>
+            <img src={team} alt=""  className='mt-30 '/>
         </div>
     );
 };
